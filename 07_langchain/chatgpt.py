b52385c01b6f0ef7619f8a4f8510ac6040b5342d
Petr Zikán <zikan@plasmasolve.com>
2024-11-13 16:30:46 +0100

    langainc test

---
+++ 07_langchain/chatgpt.py
@@ -0,0 +1,14 @@
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+chat = ChatOpenAI(temperature=0.5)
+messages = [
+    SystemMessage(
+        content="""Act as a senior software engineer at a startup company."""
+    ),
+    HumanMessage(
+        content="""Please can you provide a funny joke about software engineers?"""
+    ),
+]
+response = chat.invoke(input=messages)
+print(response.content)
